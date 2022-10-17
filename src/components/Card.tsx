22a2d17a40b2250b745403f392c75847007e4102
Robin VERDIER <robinverdier@macbook-pro-de-robin.home>
2022-10-18 01:30:35 +0200

    part-3: Add reveal action with animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,64 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { GET_CARD } from "helpers/queries";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import styled from "styled-components";
+import { useSpring, animated } from "react-spring";
+import { BarLoader } from "react-spinners";
 
 import CardType from "types/Card";
 import CardContent from "components/CardContent";
 
-type CardContainerProps = {
-  $rarity: string;
-};
-
-const CardContainer = styled.div<CardContainerProps>`
-  position: relative;
+const CardContainer = styled.div`
   flex: 1 200px;
   flex-grow: 0;
   height: 323px;
+  position: relative;
+`;
+
+const CardFace = styled(animated.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  border-radius: 10px;
+  box-shadow: 0 0 4px rgb(0 0 0 / 40%);
+`;
+
+const CardVerso = styled(CardFace)`
+  background: linear-gradient(45deg, rgb(71 126 223) 0%, rgb(52 90 193) 100%);
+  border: 5px solid #6487d8;
+  padding: 20px;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+
+const CardVersoPattern = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.2;
+  background-image: linear-gradient(
+    to right,
+    #ffffff,
+    #ffffff 0px,
+    transparent 15px,
+    transparent
+  );
+  background-size: 10px 100%;
+`;
+
+type CardRectoProps = {
+  $rarity: string;
+};
+
+const CardRecto = styled(CardFace)<CardRectoProps>`
   overflow: hidden;
   font-family: "Inter";
   font-size: 0.7rem;
   font-weight: 600;
   border-radius: 10px;
+  border: 5px solid;
 
   ${(props) => {
     switch (props.$rarity) {
@@ -32,7 +71,7 @@ const CardContainer = styled.div<CardContainerProps>`
             rgba(188, 137, 3, 1) 39%,
             rgba(241, 186, 38, 1) 100%
           );
-          border: 3px solid rgba(231, 174, 22, 1);
+          border-color: rgba(231, 174, 22, 1);
           color: #111;
         `;
       case "rare":
@@ -44,7 +83,7 @@ const CardContainer = styled.div<CardContainerProps>`
             rgba(181, 19, 15, 1) 40%,
             rgba(252, 76, 72, 1) 100%
           );
-          border: 3px solid rgba(238, 42, 37, 1);
+          border-color: rgba(238, 42, 37, 1);
           color: white;
         `;
       case "super_rare":
@@ -56,7 +95,7 @@ const CardContainer = styled.div<CardContainerProps>`
             rgba(12, 70, 163, 1) 40%,
             rgba(62, 131, 242, 1) 100%
           );
-          border: 3px solid rgba(32, 105, 224, 1);
+          border-color: rgba(32, 105, 224, 1);
           color: white;
         `;
       case "unique":
@@ -68,13 +107,13 @@ const CardContainer = styled.div<CardContainerProps>`
             rgba(22, 22, 22, 1) 39%,
             rgba(75, 75, 75, 1) 100%
           );
-          border: 3px solid rgba(48, 48, 48, 1);
+          border-color: rgba(48, 48, 48, 1);
           color: white;
         `;
       default:
         return `
           background: #eee;
-          border: 3px solid #ddd;
+          border-color: #ddd;
           color: #333;
         `;
     }
@@ -96,30 +135,58 @@ const CardImage = styled.img`
 
 type CardProps = {
   slug: string;
+  revealed: Boolean;
 };
 
-const Card = ({ slug }: CardProps) => {
-  const { error, data } = useQuery(GET_CARD, {
+const Card = ({ slug, revealed }: CardProps) => {
+  const [loadCard, { error, loading, data, called }] = useLazyQuery(GET_CARD, {
     variables: { slug },
   });
   const card: CardType = useMemo(() => data?.card, [data]);
 
+  const { transform, opacity } = useSpring({
+    opacity: revealed ? 1 : 0,
+    transform: `perspective(400px) rotateY(${revealed ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  useEffect(() => {
+    if (revealed && !called) loadCard();
+  }, [revealed, loadCard, called]);
+
   return (
-    <CardContainer $rarity={card?.rarity}>
-      {data && (
-        <>
-          <CardImage
-            src={card.player.pictureUrl}
-            alt={card.player.displayName}
-          />
-          <CardContent card={card} />
-        </>
-      )}
-      {error && (
-        <div style={{ marginTop: 100, textAlign: "center" }}>
-          An error occured while trying to load this card.
-        </div>
-      )}
+    <CardContainer>
+      <CardVerso style={{ opacity: opacity.to((o) => 1 - o), transform }}>
+        <CardVersoPattern />
+      </CardVerso>
+      <CardRecto
+        style={{
+          opacity,
+          transform,
+          rotateY: "180deg",
+        }}
+        $rarity={card?.rarity}
+      >
+        {data && (
+          <>
+            <CardImage
+              src={card.player.pictureUrl}
+              alt={card.player.displayName}
+            />
+            <CardContent card={card} />
+          </>
+        )}
+        {loading && (
+          <div style={{ width: 100, margin: "100px auto 0" }}>
+            <BarLoader color="#333333" />
+          </div>
+        )}
+        {!loading && error && (
+          <div style={{ marginTop: 100, textAlign: "center" }}>
+            An error occured while trying to load this card.
+          </div>
+        )}
+      </CardRecto>
     </CardContainer>
   );
 };
--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,8 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 import Card from "components/Card";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 const CardsContainer = styled.div`
   display: flex;
@@ -12,7 +17,22 @@ const CardsContainer = styled.div`
   padding: 25px;
 `;
 
+const RevealButton = styled.button`
+  margin: 20px auto;
+  padding: 5px 10px;
+  border: 2px solid #141414;
+  border-radius: 3px;
+  color: #141414;
+  cursor: pointer;
+  transition: background-color 0.4s;
+  &:hover {
+    color: white;
+    background-color: #141414;
+  }
+`;
+
 const CardsPage = () => {
+  const [revealed, setRevealed] = useState(false);
   const { slugs }: { slugs?: string } = useParams();
 
   const slugsArray: Array<string> = useMemo(
@@ -21,12 +41,19 @@ const CardsPage = () => {
   );
 
   return (
-    <CardsContainer>
-      {slugsArray?.map((slug) => (
-        <Card key={slug} slug={slug} />
-      ))}
-      {!slugsArray.length && <p>No slugs are specified in the URL</p>}
-    </CardsContainer>
+    <Container>
+      {!!slugsArray.length && (
+        <RevealButton onClick={() => setRevealed(!revealed)}>
+          {!revealed ? "Reveal cards" : "Hide cards"}
+        </RevealButton>
+      )}
+      <CardsContainer>
+        {slugsArray?.map((slug) => (
+          <Card key={slug} slug={slug} revealed={revealed} />
+        ))}
+        {!slugsArray.length && <p>No slugs are specified in the URL</p>}
+      </CardsContainer>
+    </Container>
   );
 };
 
